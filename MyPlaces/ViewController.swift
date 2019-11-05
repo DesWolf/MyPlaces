69275dab1050eb263cc35ac93c0ec2725173b190
Max Okuneev <okuneev@gmail.com>
2019-11-05 19:40:02 +0700

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Максим Окунеев on 11/5/19.
+//  Copyright © 2019 Максим Окунеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
