e575cd8b878aa75c6cce3d1a9414b587819e2c65
Max Okuneev <okuneev@gmail.com>
2019-11-06 11:53:23 +0700

    Add icons Set LaunchScreen Work with hight of cells

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -26,18 +26,27 @@ class MainViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       
-           return restrantNames.count
+            return restrantNames.count
        }
        
-       override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-           let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-           cell.textLabel?.text = restrantNames[indexPath.row]
-           cell.imageView?.image = UIImage(named: restrantNames[indexPath.row])
-           return cell
+        cell.textLabel?.text = restrantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restrantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
+        
+        return cell
        }
 
- 
+    // MARK: - TableViewDeligate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 85
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
