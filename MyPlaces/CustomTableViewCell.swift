c3c737b2e247df4361227974cdec293c6ab39499
Max Okuneev <okuneev@gmail.com>
2019-11-06 13:02:06 +0700

    CustomTableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Максим Окунеев on 11/6/19.
+//  Copyright © 2019 Максим Окунеев. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
