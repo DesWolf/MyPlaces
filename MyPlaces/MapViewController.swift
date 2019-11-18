8f36ec5387a0a6a9973bfa277106204e13b28941
Max Okuneev <okuneev@gmail.com>
2019-11-18 21:15:15 +0700

    Add Map in storyboard

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Максим Окунеев on 11/18/19.
+//  Copyright © 2019 Максим Окунеев. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   //     let detailVC = MapViewController()
+         
+   //      detailVC.modalPresentationStyle = .fullScreen
+   //      present(detailVC, animated: true)
+}
+    @IBAction func closeVC(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
