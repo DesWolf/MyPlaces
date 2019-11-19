1831dbdac0793a2c8be84a428fff22f925a1631a
Max Okuneev <okuneev@gmail.com>
2019-11-19 11:33:24 +0700

    Add place mark on map

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -13,15 +13,45 @@ class MapViewController: UIViewController {
 
     @IBOutlet var mapView: MKMapView!
     
+    var place: Place!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-   //     let detailVC = MapViewController()
-         
-   //      detailVC.modalPresentationStyle = .fullScreen
-   //      present(detailVC, animated: true)
+        setupPlacemark()
+   
 }
     @IBAction func closeVC(_ sender: Any) {
         dismiss(animated: true)
+    }
+    
+    private func setupPlacemark() {
+        
+        guard let location = place.location else { return }
+       
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location) { (placemarks, error) in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let placemarks = placemarks else  { return }
+            
+            let placemark = placemarks.first
+            
+            let anotation  = MKPointAnnotation()
+            anotation.title = self.place.name
+            anotation.subtitle = self.place.type
+            
+            guard let placemarkLocation = placemark?.location else { return }
+            
+            anotation.coordinate = placemarkLocation.coordinate
+            
+            self.mapView.showAnnotations([anotation], animated: true)
+            self.mapView.selectAnnotation(anotation, animated: true)
+            
+            
+        }
     }
 }
