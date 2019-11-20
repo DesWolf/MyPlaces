42a98230bd1e0af90707de9d25f533c99fbbb239
Max Okuneev <okuneev@gmail.com>
2019-11-20 10:41:24 +0700

    Add center your position button on map

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -8,21 +8,35 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
 
     @IBOutlet var mapView: MKMapView!
     
-    var place: Place!
+    var place = Place()
     let annotationIdentifier = "annotationIdentifier"
+    let locationManager = CLLocationManager()
+    let regionInMeters = 10_000.00
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupPlacemark()
+        checkLocationServices()
    
 }
     @IBAction func closeVC(_ sender: Any) {
         dismiss(animated: true)
+    }
+    
+    @IBAction func centerViewUserLocation() {
+        
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location,
+                                            latitudinalMeters: regionInMeters,
+                                            longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
     private func setupPlacemark() {
@@ -51,33 +65,95 @@ class MapViewController: UIViewController {
             
             self.mapView.showAnnotations([anotation], animated: true)
             self.mapView.selectAnnotation(anotation, animated: true)
-            
-            
         }
+    }
+    
+    private func checkLocationServices() {
+        
+        if CLLocationManager.locationServicesEnabled() {
+         setupLocationManager()
+            checkLocationAutorization()
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.showAlert(
+                        title: "Location Services are Disabled",
+                        message: "To enable it go: Settings -> Privacy -> Location Services and turn On"
+                    )
+                }
+        }
+    }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    private func checkLocationAutorization() {
+        switch CLLocationManager.authorizationStatus() {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .denied:
+           DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                          self.showAlert(
+                              title: "Your Location is Not Available",
+                              message: "To give permission Go to: Setting -> MyPlaces -> Location"
+                          )
+                      }
+            break
+        case .authorizedAlways:
+            break
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            break
+        @unknown default:
+            print("New case is avalible")
+        }
+    }
+    
+    private func showAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
-    extension MapViewController: MKMapViewDelegate {
+extension MapViewController: MKMapViewDelegate {
         
-        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
             
-            guard !(annotation is MKUserLocation) else { return  nil }
+        guard !(annotation is MKUserLocation) else { return  nil }
             
-            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
             
-            if annotationView == nil {
-                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
-                annotationView?.canShowCallout = true
-            }
-            
-            if let imageData = place.imageData {
-                let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-                imageView.layer.cornerRadius = 10
-                imageView.clipsToBounds = true
-                imageView.image = UIImage(data: imageData)
-                annotationView?.rightCalloutAccessoryView = imageView
-            }
-            
-            return annotationView
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true
         }
+            
+        if let imageData = place.imageData {
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView
+        }
+        return annotationView
     }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAutorization()
+    }
+    
+    
+    
+}
+    
 
